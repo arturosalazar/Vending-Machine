bc584c8a8f73f02c06500bf5ea3415e1a34a00f4
Arturo Salazar <arturo.v.salazar.jr@gmail.com>
2022-07-31 13:38:39 -0500

    Add toString to Item and tests to Main
    Added toString to make item fields display correctly and added tests for the functionality of creating an Item from scratch and as a copy of another item

--- Item.java
+++ Item.java
@@ -3,15 +3,47 @@ public class Item {
     private double price;
     private int quantity;
 
+    //Constructor
     public Item (String name, double price, int quantity) {
         this.name = name;
         this.price = price;
         this.quantity = quantity;
     }
 
+    //Copy Constructor
     public Item (Item source){
         this.name = source.name;
         this.price = source.price;
         this.quantity = source.quantity;
     }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public double getPrice(){
+        return this.price;
+    }
+
+    public int getQuanity(){
+        return this.quantity;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setPrice(double price){
+        this.price = price;
+    }
+
+    public void setQuantity(int quantity){
+        this.quantity = quantity;
+    }
+
+    public String toString(){
+        String output = "" + this.name + ": " + this.price + " (" + this.quantity + ")";
+        return output;
+    }
+
 }
--- Main.java
+++ Main.java
@@ -5,6 +5,22 @@ public class Main {
         System.out.println("\t             WELCOME TO JAVA DRINKS!            ");
         System.out.println("\t************************************************");
         
+        //Test 1 = make sure we can create a new item and print out correctly
+        Item pepsi = new Item("Pepsi", 1.99, 3);
+        System.out.println(pepsi);
+
+        //Test 2 = make sure we can use copy constructor to make a new item, change the new item's
+        //fields (without changing the old item) and print out both the old and new items
+        System.out.println("Adding Fresca\n");
+        Item fresca = new Item (pepsi);
+        fresca.setName("Fresca");
+        fresca.setPrice(1.49);
+        fresca.setQuantity(10);
+        System.out.println("Printing Items:");
+        System.out.println(pepsi);
+        System.out.println(fresca);
+
+
         /* Item[][] items = new Item[][] {
         /     { new Item("Pepsi", 1.99, 3) , new Item("Fresca", 1.49, 3), new Item("Brisk", 2.49, 2) },
         /     { new Item("Fanta", 1.99, 2) , new Item("Barq's", 1.49, 2), new Item("A & W", 2.49, 3) },
