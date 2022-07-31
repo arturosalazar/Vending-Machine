44dac0aae8da45d33f2ef808ce15744ad57a2a9b
Arturo Salazar <arturo.v.salazar.jr@gmail.com>
2022-07-31 13:16:11 -0500

    Add fields and constructor to Item class

--- Item.java
+++ Item.java
@@ -1,3 +1,11 @@
 public class Item {
-    
+    private String name;
+    private double price;
+    private int quantity;
+
+    public Item (String name, double price, int quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
 }
