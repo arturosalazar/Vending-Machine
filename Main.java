696c997d317fa5e9ed78e9d0ea0efe573dd206a1
Arturo Salazar <arturo.v.salazar.jr@gmail.com>
2022-08-03 13:24:44 -0500

    Add userMoney variable
    Add variable to hold the user's money. Ask the user how much they have at the beginning of the program and then use that as a basis to decide if the user can still purchase items

--- Main.java
+++ Main.java
@@ -22,6 +22,16 @@ public class Main {
         //Set up scanner
         Scanner scan = new Scanner(System.in);
 
+        //Get how much money the user
+        System.out.println("How much money do you have?");
+        double userMoney = scan.nextDouble();
+
+        //Check if user has enough money to buy the cheapest drink
+        if (userMoney < 1.49) {
+            System.out.println("I'm sorry, you do not have enough to purchase a drink. Thank you for using Java Drinks");
+            System.exit(0);
+        }
+
         while(true){
             //request a row and spot from user
             System.out.print("Pick a row: ");
@@ -30,20 +40,29 @@ public class Main {
             System.out.print("Pick a spot in the row: ");
             int spot = scan.nextInt();
 
+            //Retrieve and store the price of the selected item
+            double selectedItemPrice = vending.getItem(row, spot).getPrice();
+
+            //If the user does not have enough money to purchase the item, let user know and ask the user again
+            if (selectedItemPrice > userMoney){
+                System.out.println("I'm sorry, you don't have enough for that drink, try again");
+                continue;
+            }
+
             boolean vendingResult = vending.dispense(row, spot);
             System.out.println("\n" + vending);
 
-            //Dispense drink at requested row and spot. Then print the machine's current status
+            //If the item is available, deduct the cost from the user's money and print a message. Else explain out of item
             if (vendingResult){
-                System.out.print("\nEnjoy your drink! Press 1 to purchase another: ");
+                userMoney -= selectedItemPrice;
+                System.out.print("\nEnjoy your drink! You now have " + userMoney + " available. Press 1 to purchase another: ");
             } else {
-                System.out.print("\nSorry, we're out of this item. Press 1 to purchase another: ");
+                System.out.print("\nSorry, we're out of this item. You still have " + userMoney + " available. Press 1 to purchase another: ");
             }
             
             if (scan.nextInt() != 1){
                 break;
             }
-
         }
 
         
